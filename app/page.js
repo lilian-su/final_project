f86daa0a5bf9abf14f96b90055b509c2b3f2265b
lilian-su <liliank.497@gmail.com>
2024-07-25 20:56:40 -0600

    fixed page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { useUserAuth } from "./_utils/auth-context";
 import Link from "next/link";
 
+
 export default function SignInPage() {
 
   const { user, gitHubSignIn, firebaseSignOut} = useUserAuth(); 
@@ -28,15 +29,16 @@ export default function SignInPage() {
   return (
     <main className="p-10 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-red-300">
       <header>
-        <h1 className="text-3xl">Firebase Auth</h1>
+        <h1 className="text-3xl">Holiday Tracker</h1>
       </header>
       {user ? (
         <div>
           <p>Welcome {user.displayName}</p>
-          <p>{user.email}</p>
+          <p>You are signed in as {user.email}</p>
+
           <img className="w-8 h-8" src={user.photoURL} alt="Profile" />
           	<p>
-		  		<Link className="hover:underline decoration-wavy" href="/holiday-list">Shopping List</Link>
+		  		<Link className="hover:underline decoration-wavy" href="holiday-list">Holiday tracker</Link>
 			</p>
           <button onClick={handleSignOut} className="text-lg m-2 hover:underline">Sign Out</button>
         </div>
